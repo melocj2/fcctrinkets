f35657551ad9ef157a303640c364910f750186da
Jake Meloche <melocho@uwindsor.ca>
2019-12-05 23:04:19 -0500

    made the first button responsive

--- soundbapp/src/App.js
+++ soundbapp/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import drum from './images/drum.png'
 import smiley from './images/smiley.png'
 
+
+
 function Display() {
   return (
     <div className="Display" id="display">
@@ -23,14 +25,36 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      qPressed: false
     }
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.qClick = this.qClick.bind(this)
   }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+    document.addEventListener("keyup", this.handleKeyPress);
+  }
+
+  handleKeyPress(e) {
+    if (e.keyCode === 81)  {
+    this.setState({
+      qPressed: !this.state.qPressed
+    })}
+  }
+
+  qClick() {
+    this.setState ({
+      qPressed: !this.state.qPressed
+    })
+  }
+
   render () {
+  
   return (
-    <div className="App" id="drum-machine">
+    <div className="App" id="drum-machine" onKeyPress={this.handleKeyPress}>
       <div id="grid">
-        <button className="drum-pad" id="#">Q</button>
+        <button className="drum-pad" id="#" onMouseUp={this.qClick} onMouseDown={this.qClick} style={{backgroundImage: this.state.qPressed ? "radial-gradient(circle closest-side, rgb(206, 206, 206), rgb(169, 245, 169))" : "radial-gradient(circle closest-side, rgb(206, 206, 206), #ededed)"}}>Q</button>
         <button className="drum-pad" id="#">W</button>
         <button className="drum-pad" id="#">E</button>
         <button className="drum-pad" id="#">A</button>
