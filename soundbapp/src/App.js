5b391964cf00aaaff6e7f5e41be037e9203c7297
Jake Meloche <melocho@uwindsor.ca>
2019-12-06 13:34:50 -0500

    test switch button

--- soundbapp/src/App.js
+++ soundbapp/src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import drum from './images/drum.png'
 import smiley from './images/smiley.png'
+import freeReal from './sounds/freerealestate.mp3'
+import keepYourS from './sounds/keepYourS.mp3'
 
-
-
-function Display() {
+function Display(props) {
   return (
     <div className="Display" id="display">
       <h2 id="inputstuff">BASS DRUM</h2>
       <div id="slider1">
       <label class="switch">
   <img src={drum} alt="drum" className="logo" height="30" width="30"/>
-  <input type="checkbox" />
+  <input type="checkbox" onChange={props.toggleBoard}/>
   <img src={smiley} alt="smiley" className="logo" height="30" width="30" />
       </label>
       </div>
@@ -25,15 +25,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      qPressed: false
+      qPressed: false,
+      board: true
     }
     this.handleKeyPress = this.handleKeyPress.bind(this)
     this.qClick = this.qClick.bind(this)
+    this.toggleBoard = this.toggleBoard.bind(this)
   }
 
   componentDidMount() {
     document.addEventListener("keydown", this.handleKeyPress);
-    document.addEventListener("keyup", this.handleKeyPress);
+    
   }
 
   handleKeyPress(e) {
@@ -49,13 +51,22 @@ class App extends React.Component {
     })
   }
 
+  toggleBoard() {
+    this.setState ({
+      board: !this.state.board
+    })
+  }
+
   render () {
   
   return (
     <div className="App" id="drum-machine" onKeyPress={this.handleKeyPress}>
       <div id="grid">
-        <button className="drum-pad" id="#" onMouseUp={this.qClick} onMouseDown={this.qClick} style={{backgroundImage: this.state.qPressed ? "radial-gradient(circle closest-side, rgb(206, 206, 206), rgb(169, 245, 169))" : "radial-gradient(circle closest-side, rgb(206, 206, 206), #ededed)"}}>Q</button>
+        <button className="drum-pad" id="#" onMouseDown={this.qClick} style={{backgroundImage: this.state.qPressed ? "radial-gradient(circle closest-side, rgb(206, 206, 206), rgb(169, 245, 169))" : "radial-gradient(circle closest-side, rgb(206, 206, 206), #ededed)"}}>Q</button>
+        {this.state.qPressed && this.state.board && <audio ref="audio_tag" src={freeReal} autoPlay onEnded={this.qClick}/>}
+        {this.state.qPressed && !this.state.board && <audio ref="audio_tag" src={keepYourS} autoPlay onEnded={this.qClick}/>}
         <button className="drum-pad" id="#">W</button>
+
         <button className="drum-pad" id="#">E</button>
         <button className="drum-pad" id="#">A</button>
         <button className="drum-pad" id="#">S</button>
@@ -64,7 +75,7 @@ class App extends React.Component {
         <button className="drum-pad" id="#">X</button>
         <button className="drum-pad" id="#">C</button>
       </div>
-      <Display />
+      <Display toggleBoard={this.toggleBoard}/>
     </div>
   )
   }
